f0f48d6d97edb1a45dfbb0dbaef3a5f03bb8e832
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2025-09-28 16:31:37 -0400

    feat: add share functionality to GalleryImage component

--- app/all/(components)/image.tsx
+++ app/all/(components)/image.tsx
@@ -1,14 +1,16 @@
 'use client';
 
 import { default as NextImage } from 'next/image';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 import clsx from 'clsx';
+import { Share } from 'lucide-react';
 import type { MDXRemoteSerializeResult } from 'next-mdx-remote';
 
 import type { Image } from '@/lib/db/schema';
 
 import CustomMDX from '@/components/templates/custom-mdx';
+import { IconButton, toast } from '@/components/ui';
 
 // -----------------------------------------------------------------------------
 // Types
@@ -26,6 +28,27 @@ const GalleryImage: React.FC<GalleryImageProps> = ({ image }) => {
   const [mounted, setMounted] = useState<boolean>(false);
 
   useEffect(() => setMounted(true), []);
+
+  const isTouchScreen = mounted ? /iPhone|iPad|iPod|Android/i.test(navigator.userAgent) : false;
+
+  const share = useCallback(async () => {
+    const shareUrl = `${window.location.origin}?img=${image.index}`;
+
+    if (navigator.share) {
+      await navigator.share({
+        title: image.text ?? `friend.com review #${image.index}`,
+        url: shareUrl,
+      });
+    } else {
+      await navigator.clipboard.writeText(shareUrl);
+      toast({
+        intent: 'success',
+        title: 'Link copied to clipboard.',
+        description: shareUrl,
+      });
+    }
+  }, [image]);
+
   return (
     <a
       key={image.id}
@@ -45,7 +68,33 @@ const GalleryImage: React.FC<GalleryImageProps> = ({ image }) => {
       />
       <div
         className={clsx(
-          'absolute bottom-0 left-0 right-0 bg-black/50 px-2.5 pb-2.5',
+          'pointer-events-none absolute inset-0 h-[1.875rem] -translate-y-full bg-black/50 px-2.5 pt-2.5 font-mono text-base leading-5 text-gray-12 transition-all group-hover:translate-y-0',
+          'after:aria-hidden="true" after:pointer-events-none after:absolute after:-bottom-20 after:left-0 after:h-20 after:w-full after:bg-gradient-to-b after:from-black/50 after:to-transparent after:content-[""]',
+          isTouchScreen ? 'hidden' : 'flex',
+        )}
+      >
+        {(image.city ?? 'IMG').toUpperCase()}_{String(image.index).padStart(4, '0')}
+      </div>
+      <IconButton
+        className={clsx(
+          'absolute right-1.5 top-1.5',
+          isTouchScreen
+            ? 'flex'
+            : 'hidden group-hover:flex group-hover:animate-in group-hover:fade-in',
+        )}
+        size="md"
+        variant="ghost"
+        aria-label="Share image"
+        onClick={(e) => {
+          e.preventDefault();
+          share();
+        }}
+      >
+        <Share />
+      </IconButton>
+      <div
+        className={clsx(
+          'pointer-events-none absolute bottom-0 left-0 right-0 bg-black/50 px-2.5 pb-2.5',
           'before:aria-hidden="true" before:pointer-events-none before:absolute before:-top-20 before:left-0 before:h-20 before:w-full before:bg-gradient-to-t before:from-black/50 before:to-transparent before:content-[""]',
         )}
       >
