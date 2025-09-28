c335d582cce0bd0e85355e2e38d91db4f3603de7
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2025-09-28 02:44:26 -0400

    chore: sort alphabetically

--- components/common/location.tsx
+++ components/common/location.tsx
@@ -20,13 +20,13 @@ const Location: React.FC<LocationProps> = ({ location }) => {
     let [name, bullets, url]: [string | null, string | null, string | null] = [null, null, null];
 
     // prettier-ignore
-    if (b === 'elmhurst') [name, bullets, url] = ['Elmhurst Avenue', 'M,R', 'https://maps.app.goo.gl/Aseo9eyMTWwzBnYM7'];
-    // prettier-ignore
     if (b === '77th_st_irt') [name, bullets, url] = ['77th Street', '6', 'https://maps.app.goo.gl/a3AjbbxA71N8jC8b6'];
     // prettier-ignore
-    if (b === 'west_4th') [name, bullets, url] = ['West 4 Street–Washington Square', 'A,C,E,B,D,F,M', 'https://maps.app.goo.gl/dUjQGxSkzv7YkUCk8'];
-    // prettier-ignore
     if (b === 'dyckman_ind') [name, bullets, url] = ['Dyckman Street', 'A', 'https://maps.app.goo.gl/CxTkByPynRGk9CAd9'];
+    // prettier-ignore
+    if (b === 'elmhurst') [name, bullets, url] = ['Elmhurst Avenue', 'M,R', 'https://maps.app.goo.gl/Aseo9eyMTWwzBnYM7'];
+    // prettier-ignore
+    if (b === 'west_4th') [name, bullets, url] = ['West 4 Street–Washington Square', 'A,C,E,B,D,F,M', 'https://maps.app.goo.gl/dUjQGxSkzv7YkUCk8'];
 
     if (name && bullets) {
       if (url) {
